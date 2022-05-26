9ae68fc6ff39ac64c00b9089f877033933da7038
dangnha <nhand.21it@vku.udn.vn>
2022-05-26 19:32:24 +0700

    Best web design

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Karla: ['Karla', 'sans-serif']
+            },
+            colors: {
+                'coffee': {
+                    100: '#E8D6D0',
+                    200: '#C89F94',
+                    400: '#A25F4B',
+                    600: '#744838',
+                },
+
+                'text-footer': '#e5e7eb'
+            },
+            keyframes: {
+                slideDown: {
+                    '0%': { transform: 'translateY(-100%)' },
+                    '100%': { transform: 'translateY(0%)' },
+                },
+                fadeIn: {
+                    from: { opacity: 0 },
+                    to: { opacity: 1 }
+                }
+            },
+            animation: {
+                slideDown: 'slideDown 0.4s ease-in-out',
+                fadeIn: 'fadeIn 0.5s ease-in-out'
+
+            }
+        },
+    },
+    plugins: [],
+}
